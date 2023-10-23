fb802864bc41414f474eb1ed0f0148a43106eaba
LeonardaGregovic <leonarda.gregovic.com>
2023-10-23 18:08:58 +0000

    Commit vježbe

--- Vježba2.php
+++ Vježba2.php
@@ -1,3 +1,31 @@
 <?php
 
-$users
+$users = [ 
+    
+    ['ime' => 'Martin',
+    'prezime' => 'Kovac',
+    'godine' => '28',
+    'spol' => 'M'], 
+[
+    'ime' => 'Tina',
+    'prezime' => 'Prsa',
+    'godine' => '24',
+    'spol' => 'Ž']
+];
+
+    var_dump ($users);
+
+    unset($users[0]['spol']);
+    unset($users[1]['spol']);
+
+    
+    var_dump ($users);
+
+    $users = [
+        'ime' => 'Borna',
+    'prezime' => 'Filipovic',
+    'godine' => '29'];
+
+    var_dump ($users);
+
+    ?>
